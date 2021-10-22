d18406e8912f2a5bff4233bed69a993a5570863c
Ethan <noxinnahte@gmail.com>
2021-10-22 10:52:20 -0400

    First map text

---
+++ src/Map.java
@@ -0,0 +1,30 @@
+import
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Map {
+    String maptxt;
+
+    public Map() {
+        maptxt = generateMap("map.txt");
+    }
+    public Map(String filename) {
+
+    }
+    public String generateMap(String filename) {
+        try {
+            File file = new File("filename");
+            Scanner scan = new Scanner(file);
+            int x = 0;
+            while (scan.hasNextLine()) {
+                x++;
+            }
+            System.out.println(x);
+            return "s";
+        } catch (FileNotFoundException e) {
+            return "s";
+        }
+    }
+}
